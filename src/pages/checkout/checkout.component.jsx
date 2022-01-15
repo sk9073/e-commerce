b75640fb7a0789d70404eac44729956d383f3a43
sivanandi <59682017+sivanandi@users.noreply.github.com>
2022-01-15 13:36:55 +0530

    Added payment feature , in test mode for the moment

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../componenets/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../componenets/stripe-button/stripe-button.component';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
+
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className='checkout-page'>
@@ -31,6 +33,12 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className='total'>TOTAL: ${total}</div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
